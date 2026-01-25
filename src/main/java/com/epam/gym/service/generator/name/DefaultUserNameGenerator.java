fcd434f16202a61b1f13c85faa5652473df07e91
klimb <manulc0rparation@gmail.com>
2026-01-25 14:58:52 +0400

    fixed name generation issues for trainees and trainers

--- src/main/java/com/epam/gym/service/generator/name/DefaultUserNameGenerator.java
+++ src/main/java/com/epam/gym/service/generator/name/DefaultUserNameGenerator.java
@@ -1,24 +1,34 @@
 package com.epam.gym.service.generator.name;
 
 import com.epam.gym.GymApplication;
+import com.epam.gym.domain.user.Trainee;
 import com.epam.gym.domain.user.Trainer;
+import com.epam.gym.repository.trainee.ITraineeRepository;
 import com.epam.gym.repository.trainer.ITrainerRepository;
 import com.epam.gym.service.generator.name.factory.IUsernameFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
 @Service
 public final class DefaultUserNameGenerator implements IUsernameGenerator {
 
+    private ITraineeRepository traineeRepository;
     private ITrainerRepository trainerRepository;
     private IUsernameFactory usernameFactory;
 
     @Override
     public String generate(String firstName, String lastName) {
-        var lastSuffix = trainerRepository.findByFirstNameAndLastName(firstName, lastName)
+        var traineesUsernames = traineeRepository.findByFirstNameAndLastName(firstName, lastName)
             .stream()
-            .map(Trainer::getUsername)
-            .map(username -> username.split(GymApplication.DEFAULT_USERNAME_DELIMITER))
+            .map(Trainee::getUsername);
+        var trainersUsernames = trainerRepository.findByFirstNameAndLastName(firstName, lastName)
+            .stream()
+            .map(Trainer::getUsername);
+        var lastSuffix = Stream.concat(traineesUsernames, trainersUsernames)
+            .map(username -> username.split(Pattern.quote(GymApplication.DEFAULT_USERNAME_DELIMITER)))
             .filter(parts -> parts.length > 2)
             .map(parts -> parts[parts.length - 1])
             .mapToInt(Integer::parseInt)
@@ -26,6 +36,11 @@ public final class DefaultUserNameGenerator implements IUsernameGenerator {
         return lastSuffix.isEmpty()
             ? usernameFactory.create(firstName, lastName)
             : usernameFactory.create(firstName, lastName, lastSuffix.getAsInt());
+    }
+
+    @Autowired
+    public void setTraineeRepository(ITraineeRepository traineeRepository) {
+        this.traineeRepository = traineeRepository;
     }
 
     @Autowired
