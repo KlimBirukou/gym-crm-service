3d449c1e15286715e020d0e221b37793f4083c2b
klimb <manulc0rparation@gmail.com>
2026-02-04 14:31:55 +0400

    split divided the delimiter for tests and application

--- src/test/java/com/epam/gym/mother/UsernameMother.java
+++ src/test/java/com/epam/gym/mother/UsernameMother.java
@@ -1,20 +1,20 @@
 package com.epam.gym.mother;
 
-import com.epam.gym.GymApplication;
-
 public class UsernameMother {
 
     private static final String FIRSTNAME = "firstname";
     private static final String LASTNAME = "lastname";
 
+    private static final String DEFAULT_USERNAME_DELIMITER = ".";
+
     public static String get() {
-        return String.join(GymApplication.DEFAULT_USERNAME_DELIMITER,
+        return String.join(DEFAULT_USERNAME_DELIMITER,
             FIRSTNAME,
             LASTNAME);
     }
 
     public static String get(int suffix) {
-        return String.join(GymApplication.DEFAULT_USERNAME_DELIMITER,
+        return String.join(DEFAULT_USERNAME_DELIMITER,
             FIRSTNAME,
             LASTNAME,
             String.valueOf(suffix));
